900ea58b86e43a3731c247e0502a4789ee783562
mj46595 <mj46595>
2022-01-29 16:07:37 +0100

    wtbi1

---
+++ src/main/java/com/example/demo/Wtb1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wtb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wtb1Application.class, args);
+	}
+
+}
